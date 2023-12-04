1db3cf35b3b931a9c9dfb0185978b7a7cf70d910
hiropi1224 <hiropi1224s@gmail.com>
2023-12-04 23:48:41 +0900

    B - 1.01.出力とコメント

---
+++ apg4b/101.cpp
@@ -0,0 +1,11 @@
+// #include <bits/stdc++.h>とusing namespace std;は毎回最初に書く
+// C++のプログラムはmain関数から始まる
+// cout << "文字列" << endl;で文字列を出力できる
+// //や/* */でコメントを書ける
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "こんにちは" << endl;
+    cout << "AtCoder" << endl;
+}
