db21da0de4ad7765a52cd0e0362d0ec85f983c52
hiropi1224 <hiropi1224s@gmail.com>
2023-12-19 23:31:44 +0900

    PracticeA	Welcome to AtCoder

---
+++ AtCoder Beginners Selection/PracticeA.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+    int a, b, c;
+    string s;
+    cin >> a >> b >> c >> s;
+
+    cout << a + b + c << " " << s << endl;
+}
